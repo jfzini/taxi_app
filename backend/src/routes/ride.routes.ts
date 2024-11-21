2df7ca046ffcebb4e1dae57f4411c66f1133a647
João Felipe Zini <joao.zini@semprocesso.com.br>
2024-11-21 12:05:47 -0300

    wip: api exploration

--- backend/src/routes/ride.routes.ts
+++ backend/src/routes/ride.routes.ts
@@ -1,13 +1,13 @@
 import { Router, type Request, type Response } from 'express';
 import estimateMiddleware from '../middlewares/estimate.middleware';
+import rideController from '../controllers/ride.controller';
 
 const rideRouter = Router();
 
 rideRouter.get('/', (req: Request, res: Response) => {
   res.send('Ride route');
 });
-rideRouter.post('/estimate', estimateMiddleware, (req: Request, res: Response) => {
-  res.json({ message: 'a' });
-});
+
+rideRouter.post('/estimate', estimateMiddleware, rideController.estimateRoute);
 
 export default rideRouter;
