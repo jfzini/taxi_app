3c137e1bf3095c81d4d26263d97393d6e1f01049
João Felipe Zini <joao.zini@semprocesso.com.br>
2024-11-22 00:31:42 -0300

    feat: estimateRoute service

--- backend/src/services/ride.service.ts
+++ backend/src/services/ride.service.ts
@@ -1,5 +1,8 @@
 import { getLocationCoords, getRoutes } from '../utils/coord.util';
 
+// Model
+import RideModel from '../models/ride.model';
+
 const estimateRoute = async (origin: string, destination: string) => {
   try {
     const originCoordsPromise = getLocationCoords(origin);
@@ -10,9 +13,32 @@ const estimateRoute = async (origin: string, destination: string) => {
       destinationCoordsPromise,
     ]);
 
-    const routes = await getRoutes({ origin: originCoords, destination: destinationCoords });
-    return routes;
-  } catch (error) {}
+    const [routes] = await getRoutes({ origin: originCoords, destination: destinationCoords });
+
+    const shorterRoute = routes.routes?.sort(
+      (a, b) => Number(a.distanceMeters) - Number(b.distanceMeters),
+    )[0];
+
+    if (!shorterRoute) {
+      return { status: 404, message: 'No routes found' };
+    }
+    const distance = Number(shorterRoute.distanceMeters);
+    const drivers = await RideModel.findDrivers(distance);
+
+    const response = {
+      origin: originCoords,
+      destination: destinationCoords,
+      distance,
+      duration: shorterRoute.duration?.seconds,
+      options: drivers,
+      routeResponse: routes,
+    };
+
+    return { status: 200, response };
+  } catch (error) {
+    console.error(error);
+    return { status: 500, response: 'An error occurred while processing the request' };
+  }
 };
 
 export default { estimateRoute };
